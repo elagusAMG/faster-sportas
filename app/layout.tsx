3347e91330e82017d065bf7814e4654e99e35bb3
Agustin Moya <agustin@perlego.com>
2023-02-12 23:54:35 +0000

    Clean all the / page and make some css tweaks in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,30 @@
-import './globals.css'
+import "./globals.css";
+
+import Navbar from '@components/Navbar';
+
+export const metadata = {
+  title: {
+    default: "Sportas | We make playing sports with others easy",
+  },
+  icons: {
+    shortcut: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head />
-      <body>{children}</body>
+    <html
+      lang="en"
+      className="bg-white text-black dark:bg-[#111010] dark:text-white"
+    >
+      <body className="flex flex-col antialiased">
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
-  )
+  );
 }
